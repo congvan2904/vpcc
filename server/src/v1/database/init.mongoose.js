968a2ac25f41ed79e33ef8633f47be0226fa12d8
Cong Nguyen <nguyenvancong.nvc@gmail.com>
2022-11-10 06:50:34 +0700

    add mongo_urn to .env

--- server/src/v1/database/init.mongoose.js
+++ server/src/v1/database/init.mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://cong:1234@van-thu.rvpx6py.mongodb.net/vpcc')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('mongoose connect success'))
     .catch((err) => console.log('mongoose connect fail', err))
 
