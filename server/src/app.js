dd68d5291b90b52f268f642663e04b5b582904d9
unknown <nguyenvancong.nvc@gmail.com>
2022-11-23 08:03:28 +0700

    Create client

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const logEvents = require('./v1/helpers/logEvents')
 
 require('./v1/database/init.mongoose')
 require('./v1/database/init.redis')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
