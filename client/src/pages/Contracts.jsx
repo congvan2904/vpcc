d01f0cdcaba366f46aac0e9f6c14cc516badc2c5
unknown <nguyenvancong.nvc@gmail.com>
2022-11-29 09:27:29 +0700

    update contract

--- client/src/pages/Contracts.jsx
+++ client/src/pages/Contracts.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Helmet from "../components/Helmet";
+import instance from "../services/configAxios";
 
 const Contracts = () => {
+  const [dataContract, setDataContract] = useState([]);
+  useEffect(() => {
+    async function fetDataContract() {
+      const data = (await instance.get("contract/debt")).data;
+      if (data.message === "success") {
+        setDataContract(data.data);
+        console.log("data", data);
+      }
+    }
+    fetDataContract();
+  }, []);
+
   return (
     <Helmet title="Hợp đồng">
       <div className="contract">
@@ -52,7 +65,42 @@ const Contracts = () => {
         <div className="contract-manager-group">
           <button>Thêm hồ sơ</button>
         </div>
-        <div className="contract-show"></div>
+        <div className="contract-show">
+          <form>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Phone Number</th>
+                  <th>Email</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataContract.map((contract) => (
+                  <tr>
+                    <td>{contract.user.name}</td>
+                    <td>{contract.count}</td>
+                    <td>
+                      {contract.id_contract.map((item) => (
+                        <tr>{item}</tr>
+                      ))}
+                    </td>
+                    <td>
+                      <button type="button" onClick={(event) => {}}>
+                        Edit
+                      </button>
+                      <button type="button" onClick={() => {}}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </form>
+        </div>
       </div>
     </Helmet>
   );
