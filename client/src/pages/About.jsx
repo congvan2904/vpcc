52ea3ae9a2841875a1b11b70d83bb21f5197c482
Cong Nguyen <nguyenvancong.nvc@gmail.com>
2022-11-30 12:34:27 +0700

    add test about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import "./about.scss";
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div className="about">About</div>;
+};
 
-export default About
+export default About;
