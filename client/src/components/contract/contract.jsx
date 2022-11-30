5e1c386c9879296f42bc96da5b21ed62161550e9
unknown <nguyenvancong.nvc@gmail.com>
2022-11-30 17:06:10 +0700

    update contract ui

--- client/src/components/contract/contract.jsx
+++ client/src/components/contract/contract.jsx
@@ -3,8 +3,13 @@ const Contract = (props) => {
   const id_contract = props.contract;
   const count_contract = props.name;
   // console.log(id_contract);
+  const handleClick = (event) => {
+    if (event.detail === 2) {
+      console.log("double click", id_contract);
+    }
+  };
   return (
-    <div className="contract-main-item">
+    <div className="contract-main-item" onClick={handleClick}>
       <div className="contract-main-item-number">{id_contract}</div>
       <div className="contract-main-item-name-secretary">{count_contract}</div>
     </div>
