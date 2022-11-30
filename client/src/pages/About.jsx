b20e1ad671c70d6146b88843ad54300065b6b1c3
Cong Nguyen <nguyenvancong.nvc@gmail.com>
2022-12-01 06:41:30 +0700

    rotate text

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./about.scss";
 const About = () => {
+  const text = useRef();
+  const [chu, setChu] = useState("");
+  const t1 = "VĂN PHÒNG CÔNG CHỨNG NGUYỄN ĐỨC ĐIỀN";
+  useEffect(() => {
+    // console.log(text.current.innerText);
+    setChu(text.current.innerText.split(""));
+  }, []);
+
   return (
     <>
-      <div className="about">About</div>
-      <div className="about">About</div>
-      <div className="about">About</div>
+      <div className="about">
+        <div className="circle">
+          <p ref={text}> Nguyen Duc Dien</p>
+          {t1.split("").map((item, i) => (
+            <span key={i} style={{ transform: `rotate(${i * 9.5}deg)` }}>
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
