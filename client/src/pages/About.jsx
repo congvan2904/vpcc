1646ffde283215401d5b0cc3e54fc60945c31505
unknown <nguyenvancong.nvc@gmail.com>
2022-12-01 16:00:29 +0700

    update ui circle

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,20 +3,18 @@ import "./about.scss";
 const About = () => {
   const text = useRef();
   const [chu, setChu] = useState("");
-  const t1 =
-    "==== ⭐️ VĂN PHÒNG CÔNG CHỨNG NGUYỄN ĐỨC ĐIỀN ⭐️ ==== Văn Thư Nguyễn Văn Công ";
-  useEffect(() => {
-    // console.log(text.current.innerText);
-    setChu(text.current.innerText.split(""));
-  }, []);
+  let _do;
+  const t2 = "Văn Thư Nguyễn văn văn văn Công";
+  const t1 = ` ==== ⭐️  VĂN PHÒNG CÔNG CHỨNG NGUYỄN ĐỨC ĐIỀN  ⭐️  ==== ${t2}`;
 
+  _do = 360 / t1.length;
   return (
     <>
       <div className="about">
         <div className="circle">
           <p ref={text}> Nguyen Duc Dien</p>
           {t1.split("").map((item, i) => (
-            <span key={i} style={{ transform: `rotate(${i * 4.6}deg)` }}>
+            <span key={i} style={{ transform: `rotate(${i * _do}deg)` }}>
               {item}
             </span>
           ))}
