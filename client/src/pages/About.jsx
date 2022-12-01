40df7b2c971feb5b4ccb0d772e57859f3129824e
Cong Nguyen <nguyenvancong.nvc@gmail.com>
2022-12-01 20:58:29 +0700

    test about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -12,12 +12,17 @@ const About = () => {
     <>
       <div className="about">
         <div className="circle">
-          <p ref={text}> Nguyen Duc Dien</p>
-          {t1.split("").map((item, i) => (
-            <span key={i} style={{ transform: `rotate(${i * _do}deg)` }}>
-              {item}
-            </span>
-          ))}
+          <p style={{ color: "var(--c_green)" }} ref={text}>
+            {" "}
+            Nguyen Duc Dien
+          </p>
+          <div className="bao">
+            {t1.split("").map((item, i) => (
+              <span key={i} style={{ transform: `rotate(${i * _do}deg)` }}>
+                {item}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </>
