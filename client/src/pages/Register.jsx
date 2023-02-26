2f737aa0ae25933bbf96e972244192e065298f76
Cong <nguyenvancong.nvc2gmail.com>
2023-02-27 06:47:10 +0700

    update Ui Landing Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import "./landing.scss";
 import logo from "../assets/logo.png";
 import bgMain from "../assets/bg-nguyenducdien.jpg";
-import imgTime from "../assets/landing/time.png";
-import imgStatistical from "../assets/landing/statistical.png";
-import imgReport from "../assets/landing/report.jpg";
+import imgTime from "../assets/landing/time1.svg";
+import imgStatistical from "../assets/landing/statistical.svg";
+import imgReport from "../assets/landing/report.svg";
 import { useRef } from "react";
 import { FaPhone, FaMailBulk, FaUser } from "react-icons/fa";
 import { TiMessages } from "react-icons/ti";
@@ -33,11 +33,18 @@ const Register = () => {
           <div className="header-logo-text">
             <p>Văn phòng công chứng</p>
             <p>Nguyễn Đức Điền</p>
+            <p>An Toàn – Uy Tín – Tận Tâm</p>
+          </div>
+        </div>
+        <div className="header-address">
+          <div className="header-address-title">Địa chỉ</div>
+          <div className="header-address-main">
+            393C Nguyễn Bình, Phú Xuân, Nhà Bè
           </div>
         </div>
         <div className="header-menu">
           <div className="header-menu-item">
-            <button>Đăng Nhập</button>
+            <button className="glow-on-hover">Đăng Nhập</button>
           </div>
         </div>
         <div className="triangle-dow" onClick={handleClick}>
@@ -47,7 +54,7 @@ const Register = () => {
       <div className="sub-header"></div>
       <div
         className="landing-main"
-        style={{ backgroundImage: `url(${bgMain})` }}
+        // style={{ backgroundImage: `url(${bgMain})` }}
       >
         <div className="landing-main-title">
           <h1>
@@ -55,37 +62,51 @@ const Register = () => {
           </h1>
           <div className="content">
             <div className="content-left">
-              <h3 className="content-left-title">sự ra đời ?</h3>
-              <p>
-                Công việc văn thư với khó khăn trong việc quản lý hồ sơ.Đặc biệt
-                là những hồ sơ thư ký nợ.Khi mới vào làm thì tôi viết tay danh
-                sách hồ sơ của thư ký nào làm.Nhưng do mất quá nhiều thời gian
-                thì Chúng Tôi đã lập danh sách trên Excel và Chúng tôi hàng ngày
-                phải in danh sách những hồ sơ được phát hành.Sáng hôm sau chúng
-                tôi sẽ trả lại những hồ sơ cho thư ký.Sau khi thư kí viết bút
-                lục và ký tên thì gửi cho Chúng Tôi và Chúng tôi sẽ đánh dấu ✔.
-                Nhưng vấn đề ở đây là hồ sơ nợ từ thư ký mỗi lần đưa cho văn thư
-                có khi lên tới 150 bộ.Khiến công việc kiểm tra trên giấy tờ rất
-                khó và nhiều khi ✔ sai.Đồng thời để tìm thư ký đã nợ bao nhiêu
-                hồ sơ thì rất khó.Mỗi khi Sếp hỏi hồ sơ nợ của thư ký thì Chúng
-                Tôi rất khó để trả lời.Vì vậy Chúng Tôi đã nghĩ đến việc quản lý
-                hồ sơ nợ bằng phần mền sẽ nhanh chóng và tiện lợi hơn so với
-                việc quản lý hồ sơ nợ trên gấy tờ.Đó là lý do Phần Mền Văn Thư
-                Công Chứng ra đời với 1 mục tiêu duy nhất là để nhanh chóng trả
-                lời số hồ sơ mà thư ký đã nợ là bao nhiêu và tổng số hồ sơ nợ là
-                bao nhiêu mỗi khi Sếp hỏi.
-              </p>
+              <h3 className="content-left-title">
+                Khó khăn khi quản lý hồ sơ nợ bằng cách viết tay
+              </h3>
+
+              <div className="content-left-main">
+                <div className="main-sub">
+                  - Một ngày có tầm hơn 20 bộ hồ sơ => viết tay rất lâu.
+                </div>
+                <div className="main-sub">
+                  - Khi kiểm tra hồ sơ thì dễ sai sót trong việc ✔ => Phải cẩn
+                  thận thì mất thời gian.
+                </div>
+                <div className="main-sub">
+                  - Thống kê số lượng hồ sơ nợ => Rất khó khăn => Mất rất nhiều
+                  thời gian.
+                </div>
+                <div className="main-sub">
+                  - Mỗi lần báo cáo với Sếp thì lại là 1 lần thống kê => Rất khó
+                  khăn.
+                </div>
+                <div className="main-sub">
+                  - Việc báo cáo không linh động. Phải lên gặp trực tiếp Sếp để
+                  báo cáo hoặc đánh máy rồi gửi Sếp thì cũng mất thời gian.
+                </div>
+              </div>
             </div>
             <div className="content-middle">
               <p>Văn Thư</p>
             </div>
             <div className="content-right">
-              <p>
-                Phần mềm văn thư công chứng ra đời với mục tiêu là : 1)Đánh dấu
-                những thư ký đã trả hồ sơ 2)Liệt kê những thư ký nợ hồ sơ và nợ
-                những bộ nào 3)Thống kê bằng biểu đồ và số liệu để tiện báo cáo
-                với Sếp
-              </p>
+              <h3 className="content-right-title">sự ra đời ?</h3>
+              <div className="content-right-main">
+                <div className="content-right-main-intro">
+                  {/* Phần mềm văn thư công chứng ra đời với mục tiêu là : */}
+                </div>
+                <div className="main-sub">
+                  1)Đánh dấu những hồ sơ mà thư ký đã trả
+                </div>
+                <div className="main-sub">
+                  2)Liệt kê những thư ký nợ hồ sơ và nợ những bộ nào
+                </div>
+                <div className="main-sub">
+                  3)Thống kê bằng biểu đồ và số liệu để tiện báo cáo với Sếp
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -100,19 +121,19 @@ const Register = () => {
               className="time"
               style={{ backgroundImage: `url(${imgTime})` }}
             >
-              Thời gian
+              Tiết kiệm thời gian
             </div>
             <div
               className="statistical"
               style={{ backgroundImage: `url(${imgStatistical})` }}
             >
-              Thống kê
+              Thống kê nhanh chóng
             </div>
             <div
               className="report"
               style={{ backgroundImage: `url(${imgReport})` }}
             >
-              Báo cáo
+              Báo cáo dễ dàng
             </div>
           </div>
         </div>
@@ -165,5 +186,5 @@ const Register = () => {
     </div>
   );
 };
-
+//tiet kiem tgian, thong ke nhanh chong, bao cao de dang
 export default Register;
