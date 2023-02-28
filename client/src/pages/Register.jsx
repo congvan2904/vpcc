a4a0a6d3ddfc9c484c4b2126b3170f78171f79fc
unknown <nguyenvancong.nvc@gmail.com>
2023-02-28 15:19:26 +0700

    map

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -183,6 +183,16 @@ const Register = () => {
           </div>
         </div>
       </div>
+      <div className="map">
+        <iframe
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=393c nguyen binh&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
+      </div>
     </div>
   );
 };
