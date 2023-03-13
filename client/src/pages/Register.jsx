39f460891926e6b5a7e28467312a2a977138fbc3
unknown <nguyenvancong.nvc@gmail.com>
2023-03-13 15:39:18 +0700

    test z-index

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,26 @@
+import "./register.scss";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="bao">
+      <div className="cha">
+        <div className="con1">
+          <div className="con1-text">
+            Day la con cua 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+          </div>
+        </div>
+        <div className="con2">
+          <div className="con2-text">
+            Day la con cua 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+          </div>
+        </div>
+        <div className="con3">
+          <div className="con3-text">
+            Day la con cua 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
